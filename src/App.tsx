1584f94582fb05648079bb635b91f19d3bcec9de
Mateusz <matebrzozowski@gmail.com>
2024-08-19 09:31:04 +0200

    Add Tasks page with Kanban board and drag-and-drop functionality
    Adds a new page displaying tasks in a Kanban board format.Implements drag-and-drop functionality for tasks between stages.Includes basic task removing  and redirect to edit page features.

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
+import TasksList from "./pages/tasks/list";
 
 
 
@@ -80,6 +81,13 @@ function App() {
                         <Route index  element={<CompanyList />}/>
                         <Route path="new"  element={<CompanyCreate />}/>
                         <Route path="edit/:id"  element={<CompanyEdit />}/>
+
+                      </Route>
+
+                      <Route path="/tasks"  >
+                        <Route index  element={<TasksList />}/>
+                        {/* <Route path="new"  element={<CompanyCreate />}/>
+                        <Route path="edit/:id"  element={<CompanyEdit />}/> */}
 
                       </Route>
 
